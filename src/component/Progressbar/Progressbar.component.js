a1d293f27e0ef655a826a1eb974b2a9e713639df
Martins Meisters <mart8ins@gmail.com>
2023-01-16 13:45:03 +0200

    completed test commit after deadline

--- src/component/Progressbar/Progressbar.component.js
+++ src/component/Progressbar/Progressbar.component.js
@@ -1,21 +1,100 @@
-// import PropTypes from 'prop-types';
 import { PureComponent } from "react";
-
 import "./Progressbar.style";
 
 export class Progressbar extends PureComponent {
+  state = {
+    allSteps: [],
+  };
+
+  componentDidMount() {
+    const progressState = this.props.allSteps.map((step, i) => {
+      const filledStep = i === 0 ? true : false;
+      return {
+        step: step.step,
+        title: step.title,
+        completed: false,
+        filled: filledStep,
+      };
+    });
+    this.setState({ allSteps: progressState });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.checkoutStep !== this.props.checkoutStep) {
+      const update = this.state.allSteps.map((step) => {
+        if (step.step === this.props.checkoutStep) {
+          return {
+            step: step.step,
+            completed: false,
+            filled: true,
+            title: step.title,
+          };
+        }
+        if (step.step === prevProps.checkoutStep) {
+          return {
+            step: step.step,
+            completed: true,
+            filled: true,
+            title: step.title,
+          };
+        }
+        return {
+          ...step,
+        };
+      });
+      this.setState({
+        allSteps: update,
+      });
+    }
+  }
+
   renderProgressbar() {
-    const { checkoutStep, allSteps } = this.props;
-    console.log(checkoutStep);
     return (
       <div block="ProgressbarContainer">
-        <div block="Line"></div>
+        <div block="SideLines LeftLine SelectedLine"></div>
         <div block="Steps">
-          {allSteps.map((step, i) => {
-            return <div block="Step">{step}</div>;
+          {this.state.allSteps.map((step, i) => {
+            return (
+              <>
+                {i !== 0 && i !== this.state.allSteps.length - 1 && (
+                  <div
+                    block={`BetweenLine ${step.filled && "SelectedLine"}`}
+                  ></div>
+                )}
+
+                {i !== this.state.allSteps.length - 1 && (
+                  <div block="Step">
+                    <div
+                      block={`StatusCircle ${step.filled && "FilledCircle"}`}
+                    >
+                      {step.completed ? (
+                        <div block="checkMark"></div>
+                      ) : (
+                        <div block="stepNumber">{i + 1}</div>
+                      )}
+                    </div>
+                    <div block={`stepTitle`}>
+                      <span
+                        block={`stepTitleSpan ${
+                          step.filled ? "SelectedStepTitle" : null
+                        }`}
+                      >
+                        {step.title}
+                      </span>
+                    </div>
+                  </div>
+                )}
+              </>
+            );
           })}
         </div>
-        <div block="Line"></div>
+        <div
+          block={`SideLines RightLine ${
+            this.state.allSteps.length &&
+            this.state.allSteps[this.state.allSteps.length - 2].completed &&
+            "SelectedLine"
+          }`}
+        ></div>
       </div>
     );
   }
